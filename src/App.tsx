a7eff886060c94aff44cbbfc51a0448ba6eb4adf
Romcha Levedev <lr.mos@mail.ru>
2022-10-16 12:21:41 +0300

    added lesson 18

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,19 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {NavBar} from "./components/NavBar/NavBar";
 import {Profile} from "./components/Profile/Profile";
+import {Dialogs} from "./components/Dialogs/Dialogs";
+
 
 function App() {
     return (
         <div className="app-wrapper">
             <Header/>
             <NavBar/>
-            <Profile/>
+            {/*<Profile/>*/}
+            <div className='app-wrapper-content'>
+                <Dialogs />
+            </div>
+
         </div>
     );
 }
---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import classes from './Dialogs.module.css';
+
+
+export const Dialogs = () => {
+    return (
+        <>dialog</>
+    )
+}
