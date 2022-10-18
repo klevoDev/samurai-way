5d06d5f08a16d3278ac60e645646d4c038ad0b76
Romcha Levedev <lr.mos@mail.ru>
2022-10-18 20:54:32 +0300

    added lesson 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,6 +4,36 @@ import classes from './Dialogs.module.css';
 
 export const Dialogs = () => {
     return (
-        <>dialog</>
+        <div className={classes.dialogs}>
+            {/*<h1>Dialog</h1>*/}
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Dima
+                </div>
+                <div className={classes.dialog}>
+                    Max
+                </div>
+                <div className={classes.dialog}>
+                    Polina
+                </div>
+                <div className={classes.dialog}>
+                    Miron
+                </div>
+                <div className={classes.dialog}>
+                    Volodya
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>
+                    Hi
+                </div>
+                <div className={classes.message}>
+                    how are you?
+                </div>
+                <div className={classes.message}>
+                    was the lesson great yesterday, did you do your homework?
+                </div>
+            </div>
+        </div>
     )
 }
